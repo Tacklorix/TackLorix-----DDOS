56886a9757f58d454d5663adf1a75be67f836fd8
TackLorix <157905537+Tacklorix@users.noreply.github.com>
2024-07-17 04:32:49 -0700

    Create DDOS3.sh

---
+++ DDOS3.sh
@@ -0,0 +1,54 @@
+Black="\033[1;30m"       # Black
+Red="\033[1;31m"         # Red 
+Green="\033[1;32m"       # Green
+Yellow="\033[1;33m"      # Yellow
+Blue="\033[1;34m"        # Blue
+Purple="\033[1;35m"      # Purple
+Cyan="\033[1;36m"        # Cyan
+White="\033[1;37m"       # White
+
+clear
+
+#tack
+    echo " "
+    echo -e "$Red
+   ⠀    ⠀⢻⣿⣿⣿⣿⣦
+   ⠀    ⠀⠀⢿⣿⣿⣿⣿⣷⣄⣀⣀⣀⣀⣀⣀
+   ⠀    ⣠⣴⣾⣿⣿⣿⣿⣿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠷⠶⠶⠤⢤⣀
+   ⠀    ⣿⣿⣿⣿⣿⣿⣿⡇$Yellow⠀⠀⠀⠀⠀⠀DDOS$Red⠀ ⠀⠀⠀⠀⣿⣿⣷⣦⣄⡀
+   ⠀    ⣿⣿⣿⣿⣿⣿⣿⡇$Yellow⠀⠀⠀professional$Red⠀⠀⣿⣿⣿⣿⠿⠛
+   ⠀    ⠿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⠶⠟⠛⠉
+⠀⠀   ⠀    ⣸⣿⣿⣿⣿⣿⠟⠛⠛⠛⠛⠛⠛⠛⠛⠉⠉⠉
+   ⠀    ⢰⣿⣿⣿⣿⡿⠃
+⠀   ⠀  ⠉⠉⠉⠉⠉
+
+$Cyan  Advanced DDOS Guide for determine  $Red($Cyan port & level DDOS$Red )$Cyan for DDOS to target IP 
+
+$Purple  sample Instruction$Red :$Green python DDOS-tacklorix.py -i □ -p ■ -l ●
+
+$Purple instead □$Red :$Green enter IP site Target 
+
+$Purple instead ■$Red :$Green enter desired Port 
+
+$Purple instead ●$Red :$Green  enter Level DDOS to site
+
+$Green  />$Yellow If you understand  enter 1  Then enter the command according to the guide  for start ddos$Red :$White 
+    "
+
+    read ch
+   if [ $ch -eq 1 ];then 
+
+        exit
+    elif [ $ch -eq 2 ];then 
+   
+        
+        exit
+    elif [ $ch -eq 3 ];then
+    
+        
+    
+    else
+        echo -e "\e[4;32m Invalid Input !!! \e[0m"
+        pause #lorix
+    fi
+done
